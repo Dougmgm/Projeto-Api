d78a669a840bbc1a763063bded93baf40e32fef2
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2023-01-31 17:08:00 -0300

    Add files via upload

---
+++ Migrations/20230131123059_Remoção de atributo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remoçãodeatributo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValorTotal",
+                table: "Pedidos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "ValorTotal",
+                table: "Pedidos",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
